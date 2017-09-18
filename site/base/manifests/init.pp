726c5ac13ff9e0059127e8e58d103ecd83818c94
Alexey Zhigaev <zhigaev@dultonmedia.com>
2017-09-18 16:37:33 +0300

    Added production branch

--- site/base/manifests/init.pp
+++ site/base/manifests/init.pp
@@ -15,8 +15,8 @@ class base inherits base::params {
   include base::limits
   include base::logwatch
 
-  base::user {'testuser':
-    ssh_pub_key		=> 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDpznYy/qmLfkbboSs5KrXmJoaZ5MoRIPuI+j4nRqVVezyfjkV1meQ3eHyNQzk5GAwhZmFyu9B2v/BUyUEing2c1tEPxLySH3wAoBcgbC20eXDiUyYbVCDB7xiC57sw8VvidfCzvpneIo0mfYnTfOiWbskL5M8j28T/f/YE4ZA3lwFvSu1ZhyYagLBYrl/ZCgOSEQRUhYgOM1MshQu+kIB6/Kxz+a3xBXO9GQqfKyZ339dLPFoe0QJrB/8ARuPIOkJuaR4Zr7RcflhB3ofxv2hN6kO4vxqVJLXTBulHjG7NAc0it8DGHwfrv54ZPWQGCOOauELdlognp0zYMeUxUxg1',
-    groups		=> ['group1', 'group2'],
-  }
+#  base::user {'testuser':
+#    ssh_pub_key		=> 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDpznYy/qmLfkbboSs5KrXmJoaZ5MoRIPuI+j4nRqVVezyfjkV1meQ3eHyNQzk5GAwhZmFyu9B2v/BUyUEing2c1tEPxLySH3wAoBcgbC20eXDiUyYbVCDB7xiC57sw8VvidfCzvpneIo0mfYnTfOiWbskL5M8j28T/f/YE4ZA3lwFvSu1ZhyYagLBYrl/ZCgOSEQRUhYgOM1MshQu+kIB6/Kxz+a3xBXO9GQqfKyZ339dLPFoe0QJrB/8ARuPIOkJuaR4Zr7RcflhB3ofxv2hN6kO4vxqVJLXTBulHjG7NAc0it8DGHwfrv54ZPWQGCOOauELdlognp0zYMeUxUxg1',
+#    groups		=> ['group1', 'group2'],
+#  }
 }
